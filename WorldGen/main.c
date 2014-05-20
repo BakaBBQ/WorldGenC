bd90b0914bf2667c679816f8611aa3e543166abf
BakaBBQ <laoboqi@126.com>
2014-05-20 16:49:22 -0700

    Added
    fine now doing  it for c

--- WorldGen/main.c
+++ WorldGen/main.c
@@ -2,12 +2,14 @@
 //  main.c
 //  WorldGen
 //
-//  Created by Cheryl on 5/20/14.
+//  Created by LBQ on 5/20/14.
 //  Copyright (c) 2014 Boqi Inc. All rights reserved.
 //
 
 #include <stdio.h>
 
+
+static int map[200][200];
 int main(int argc, const char * argv[])
 {
 
