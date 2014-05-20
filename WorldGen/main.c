aa41a24d1332d8c6b1d0fb0ef52f9c75f4c563b4
BakaBBQ <laoboqi@126.com>
2014-05-20 16:47:55 -0700

    Initial Commit

---
+++ WorldGen/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  WorldGen
+//
+//  Created by Cheryl on 5/20/14.
+//  Copyright (c) 2014 Boqi Inc. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
