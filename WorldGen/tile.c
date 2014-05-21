4e35b0b3d322d22f98adbf2b93fae38f6e3ed46a
BakaBBQ <laoboqi@126.com>
2014-05-20 21:31:29 -0700

    wtf is this
    this looks so wtf

--- WorldGen/tile.c
+++ WorldGen/tile.c
@@ -11,5 +11,34 @@
 #define HEIGHT 200
 typedef struct {
     int id;
-    
+    char type[2]; //something like A1, A2, B, C blah
+    int shape; //wtf a lot of binary numbers
 }Tile;
+
+
+
+
+int tile_SetTileId(char *set, int index, int shape){
+    int result = 0;
+    if (strcmp(set, "A1") == 0 )
+    {
+        result = 2048 + index * 48 + shape;
+    }else if(strcmp(set, "A2")==0){
+        result = 2816 + index * 48 + shape;
+    }else if(strcmp(set, "A3")==0){
+        result = 4352 + index * 48 + shape;
+    }else if(strcmp(set, "A4")==0){
+        result = 5888 + index * 48 + shape;
+    }else if(strcmp(set, "A5")==0){
+        result = 1536 + index;
+    }else if(strcmp(set, "B")==0){
+        return index;
+    }else if(strcmp(set, "C")==0){
+        return 256 + index;
+    }else if(strcmp(set, "D")==0){
+        return 512 + index;
+    }else if(strcmp(set, "E")==0){
+        return 768 + index;
+    }
+    return result;
+}
