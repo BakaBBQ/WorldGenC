6ac7657c1c3f13398fb01e547efb179de9ab4c17
BakaBBQ <laoboqi@126.com>
2014-05-20 19:26:00 -0700

    =A=?
    I do not even know what happened

--- WorldGen/main.c
+++ WorldGen/main.c
@@ -6,13 +6,22 @@
 //  Copyright (c) 2014 Boqi Inc. All rights reserved.
 //
 
+/*
+ actually i am feeling really bad when writing C code
+ I feel like i am doing a lot of things and codings but
+ i feel like i am wasting time
+ 
+ at least it is more efficient._.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #define WIDTH 30
 #define HEIGHT 30
 #define TOTALSTEPS 2
-
+static int die = 0;
+static int live = 1;
 
 int init_state();
 int print_state();
@@ -20,15 +29,19 @@ static int map[WIDTH][HEIGHT] = {{0}};
 static int cmap[WIDTH][HEIGHT] = {{0}};
 int main(int argc, const char * argv[])
 {
+    printf("test");
     init_state();
-    print_state();
+    //step();
+    //step();
+    //step();
     return 0;
 }
 
 int init_state(){
-    srand(time(NULL));
+    //srand(time(NULL));
     int r;
     for (int i = 0; i < WIDTH; i ++){
+        printf("iterating...");
         for (int j = 0; j < HEIGHT; j ++){
             r  = rand() % 4;
             if(r == 1){
@@ -43,6 +56,7 @@ int init_state(){
 }
 
 int step(){
+    print_state();
     memcpy(cmap, map, sizeof(map));
     for (int i = 0; i < WIDTH; i ++){
         for (int j = 0; j < HEIGHT; j ++){
@@ -52,10 +66,11 @@ int step(){
     return 0;
 }
 // cellStep, called to every cell on the map
-static die = 0;
-static live = 1;
+
 int cellStep(int x, int y){
     int cell = map[x][y];
+    int cnt = livingNeighborCnt(x,y);
+    int result;
     if(cell == live){ //Living Cell
         // get the living neighbors
         /*
@@ -65,18 +80,20 @@ int cellStep(int x, int y){
          Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
          */
         
-        int cnt = livingNeighborCnt(x,y);
-        int result;
+        
         if (cnt < 2){
             result = die;
         } else if (cnt >= 2 && cnt <= 3){
             result = live;
         } else{
-            result = live; // cruel life
+            result = die; // cruel life
         }
     }else{ //Dead Cell
-        
-    }
+        if (cnt == 3){
+            result = live;
+        }
+    } // end of deciding the fate of the cell
+    cmap[x][y] = result;
     return 0;
 }
 
@@ -87,10 +104,10 @@ int livingNeighborCnt(int x, int y){
             int tx = x + i;
             int ty = y + j;
             if (i == 0 && j == 0){
-                continue;
+                //continue;
             } else if (tx < 0 && ty < 0){ // when the cell does not exist
-                continue;
-            } else { //When the cell exists and it is not itself
+                //continue;
+            } else if(map[tx][ty] == live){ //When the cell exists and it is not itself
                 cnt ++;
             }
         } // Iterating....
@@ -106,6 +123,7 @@ int print_state(){
         }
         printf("\n");
     }
+    printf("===================");
     return 0;
 }
 
